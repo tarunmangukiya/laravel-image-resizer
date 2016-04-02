8c0766179ade9b0b2cf3e1cdf98b158b6db96fd2
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2016-04-02 13:12:59 +0530

    Resizing as a Laravel Job, Config documented
    - Resize image using laravel job queue
    - Config is edited according to new version
    - Minor bug fixes in case of retriving images
    - Service Provider Facades access name change

--- src/config/config.php
+++ src/config/config.php
@@ -4,31 +4,89 @@ return array(
 
     /*
     |--------------------------------------------------------------------------
-    | Image Driver
+    | Queue
     |--------------------------------------------------------------------------
     |
-    | Intervention Image supports "GD Library" and "Imagick" to process images
-    | internally. You may choose one of them according to your PHP
-    | configuration. By default PHP's "GD Library" implementation is used.
+    | Whether the image resizing process should be queued
     |
-    | Supported: "gd", "imagick"
+    | Default: false
     |
     */
 
-    'dynamic_generate' => true,
+    'queue' => false,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Queue Name
+    |--------------------------------------------------------------------------
+    |
+    | If queue is set to true, queue_name will be used to push the jobs in queue
+    |
+    | Default: 'imageresizer'
+    |
+    */
+
+    'queue_name' => 'imageresizer',
+    
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dynamic Generate
+    |--------------------------------------------------------------------------
+    |
+    | Provides the support to generate the images dynamically
+    | when the request is called by user.
+    | Generally used when we are using queue to resize images,
+    | it provides the support to generate and show resized images on the go,
+    | despite of the job is executed or not.
+    | Also can be useful when we want all our images to be re-generated on the go.
+    |
+    | Default: 'false'
+    |
+    */
+
+    'dynamic_generate' => false,
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Types of sizes
+    |--------------------------------------------------------------------------
+    |
+    | Defines the types of sizes in which image has to be resized and stored.
+    |
+    | Appendix:
+    | 'profile' => type of the image (you can give any name)
+    | 'crop' => 'enabled' => whether the image has to be cropped or not,
+    |         'uncropped_image' => path (where the uncropped image has to be saved)
+    |                              not required, (if we do not want to save original image)
+    | 'compiled' => path where the compiled (resized) images will be saved
+    | 'default' => default image file (that has to be retured in case of image not found)
+    |                                 (can be useful for setting default profile picture)
+    | 'sizes' => sizes of the images with key that has to be resized
+    |          'small' (any name can be used)
+    |          format: [width, height, ['fit|stretch', ['file type: original, jpg, png, gif', ['animated: if gif image has to be animated in resizing format'] ] ] ]
+    |
+    |
+    | Don't use public_path() function in case of image has to be stored in public folder.
+    | (see, 'compiled', 'default' type values)
+    |
+    |
+    */
 
     'types' => [
         'profile' => [
             'original' => storage_path() . '/profile',
             'crop' => [
-                'enabled' => true,
+                'enabled' => false,
                 'uncropped_image' => storage_path() . '/profile/uncropped',
             ],
             'compiled' => 'images/profile',
             'default' => 'images/profile-default.jpg',
             'sizes' => [
-                'small' => [100, 100, 'fit'],
-                'normal' => [250, 250, 'stretch']
+                'small' => [100, 100, 'fit', 'jpg'],
+                'normal' => [300, null, 'fit', 'original', 'animated'],
+                'large' => [400, null, 'stretch']
             ]
         ],
     ],
